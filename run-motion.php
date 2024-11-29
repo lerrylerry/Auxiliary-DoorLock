d797aaffb0837ace89d62f59610bdee0034d72f8
Nedzzz <nedrickallen.baybay@gsfe.tupcavite.edu.ph>
2024-11-29 15:36:31 +0800

    run motiondetect

--- run-motion.php
+++ run-motion.php
@@ -1,9 +1,3 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$pythonScriptPath = 'Auxiliary/motion-detection.py';
-$output = shell_exec("python " . escapeshellarg($pythonScriptPath));
-echo "<pre>$output</pre>";
+phpinfo();
 ?>
