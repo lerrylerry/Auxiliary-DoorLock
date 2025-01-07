441dff51526286cdf08498d26f3eef47586624d1
Nedzzz <nedrickallen.baybay@gsfe.tupcavite.edu.ph>
2025-01-07 13:37:20 +0800

    revision 2025

--- parser.php
+++ parser.php
@@ -6,33 +6,25 @@ if (isset($_GET['pincode'])) {
     // Prepare SQL to get the pincode details from tbup
     $sqlgetpin = "SELECT name, status FROM tbup WHERE pincode = '" . $_GET['pincode'] . "' AND dooraccess = 'Approved'";
     $sqlnullpin = "UPDATE tbup SET dooraccess = NULL WHERE pincode = '" . $_GET['pincode'] . "'";
-
+    $sqlMaster = "SELECT pincodes FROM tbparser WHERE id = 1";
+    
     // Query the tbup table
+    $resultMaster = mysqli_query($db, $sqlMaster);
     $result = mysqli_query($db, $sqlgetpin);
     $nullpin = mysqli_query($db, $sqlnullpin);
     $data = mysqli_fetch_array($result);
+    $data2 = mysqli_fetch_array($resultMaster);
 
-    // If no result is found with the given pincode in tbup
-    if (empty($data)) {
-        // Query the tbparser table to get the dynamic 'master' value for id = 1
-        $sqlMaster = "SELECT pincodes FROM tbparser WHERE id = 1";
-        $resultMaster = mysqli_query($db, $sqlMaster);
+    if (!empty($data)) {
+        echo "startname;" . $data [0] . ";endname;startstatus;" . $data [1] . ";endstatus;";
 
-        // If a result is found for the 'master' value in tbpars er
-        if (($_GET['pincode']) === $resultMaster ) {
-            $row = mysqli_fetch_assoc($resultMaster);
-            $masterName = "Master" ;// Get the 'master' value from tbparser
-
-            // Output the dynamic values
-            echo "startname;{$masterName};endname;startstatus;active;endstatus;";
-        } else {
-            // In case there's no 'master' in tbparser, return a no result message
-            echo "nothing;noresult";
-        }
     } else {
-        // If data is found for the given pincode, return the name and status from tbup
-        echo "startname;" . $data[0] . ";endname;startstatus;" . $data[1] . ";endstatus;";
-    }
+	    if (($_GET['pincode']) === $resultMaster) {
+	    echo "startname;Master;endname;startstatus;active;endstatus;";
+	} else {
+            echo "nothing;noresult";
+ 	 }
+	}
 }
 
 ?>
